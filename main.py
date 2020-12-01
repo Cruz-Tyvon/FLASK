3b33bad2fa638605b3c462f75f5420ddbc6f4972
Tyvon Cruz <cruz_tyvon@columbusstate.edu>
2020-12-01 07:45:29 +0000

    updated

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, url_for
+from jinja2 import Template
 
 app = Flask(__name__)
 
@@ -10,6 +11,11 @@ def index():
 def hello():
     return 'Hello, World'
 
+
+template = Template('Hello {{ name }}!')
+template.render(name='John Doe')
+
 with app.test_request_context():
     print(url_for('index'))
     print(url_for('hello'))
+url_for('static', filename='style.css')
